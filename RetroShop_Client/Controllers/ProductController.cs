716fe34c3f7527f0f91613b19291cdf235d16470
Sol Cardozo <43156596+MVelasquez98@users.noreply.github.com>
2022-09-02 16:30:42 -0300

    grpc client cambios en la arquitectura

---
+++ RetroShop_Client/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using RetroShop_Client.Protos;
+
+namespace RetroShop_Client.Controllers
+{
+    [Route("api/producto")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string GrpcChannelURL = "https://localhost:7110";
+        #region mocks
+        private List<ProductoDTO> _products = new List<ProductoDTO>();
+        //private readonly ServerResponse serverResponseOk = new ResProductoDTO()
+        //{
+        //};
+        private readonly List<CategoriaDTO> _categorias = new List<CategoriaDTO>()
+        {
+            new CategoriaDTO() { IdCategoria=1, Categoria="Vintage" }
+        };
+        #endregion
+
+        #region endpoints
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductoDTO producto)
+        {
+            try
+            {
+                //using var channel = GrpcChannel.ForAddress(GrpcChannelURL);
+                //var client = new ProductoService.ProductoServiceClient(channel);
+                //var response = client.Post(producto);
+                _products.Add(producto);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+                //return BadRequest(new ResProductoDTO() { Producto = null, Response = new ServerResponse() { Code = 400, Msg = "Error" } });
+            }
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{idProducto}")]
+        public void Put(int idProducto, [FromBody] ProductoDTO producto)
+        {
+        }
+        //GET api/<ProductController>/
+        [HttpGet]
+        public async Task<ActionResult> GetByFilter([FromBody] ProductoFilterDTO productoFilter)
+        {
+            return Ok(_products);
+        }
+        //GET api/<ProductController>/
+        [HttpGet]
+        [Route("categoria")]
+        public async Task<ActionResult> GetAllCategorias()
+        {
+            return Ok(_categorias);
+        }
+        #endregion
+    }
+}
---
+++ RetroShop_Client/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using RetroShop_Client.Model.Config;
+using RetroShop_Client.Protos;
+
+namespace RetroShop_Client.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IOptions<ApiConfig> _config;
+        private UsuarioService.UsuarioServiceClient _service;
+        #region mocks
+        private List<UsuarioDTO> _usuarios = new List<UsuarioDTO>();
+        private readonly UsuarioDTO usuarioResponse = new UsuarioDTO()
+        {
+            Apellido = "Velasquez",
+            Nombre = "Matias",
+            Dni = "41716615",
+            Email = "matiasvelasquez840@gmail.com",
+            Clave = "pass"
+        };
+
+        public UserController(IOptions<ApiConfig> config)
+        {
+            _config = config;
+            using var channel = GrpcChannel.ForAddress(_config.Value.GrpcChannelURL);
+            _service = new UsuarioService.UsuarioServiceClient(channel);
+        }
+
+
+        #endregion
+
+
+        #region endpoints
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UsuarioDTO usuario)
+        {
+            try
+            {
+                //var response = _service.addUsuario(usuario);
+                _usuarios.Add(usuario);
+                return Ok(_usuarios.Count);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+        //GET
+        [HttpGet]
+        [Route("login")] public async Task<ActionResult> Login([FromBody] GetByUsuarioYClaveRequest usuarioLogin)
+        {
+            
+            //var response = _service.getByUsuarioYClaveRequest(usuarioLogin);
+            return Ok(usuarioResponse);
+        }
+        #endregion
+    }
+}
