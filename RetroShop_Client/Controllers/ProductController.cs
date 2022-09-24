0bb3d5b8dd0125e5d87e2f9230c015fcaa212c76
Sol Cardozo <43156596+MVelasquez98@users.noreply.github.com>
2022-09-24 13:32:28 -0300

    cambios en controller Put api producto

--- RetroShop_Client/Controllers/ProductController.cs
+++ RetroShop_Client/Controllers/ProductController.cs
@@ -25,7 +25,7 @@ namespace RetroShop_Client.Controllers
         {
             _config = config;
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            GrpcChannel channel = GrpcChannel.ForAddress(_config.Value.GrpcChannelURLProducto);
+            GrpcChannel channel = GrpcChannel.ForAddress(_config.Value.GrpcChannelURL);
             _service = new ProductoService.ProductoServiceClient(channel);
         }
         #endregion
@@ -46,7 +46,10 @@ namespace RetroShop_Client.Controllers
                     CantidadDisponible = producto.cantidadDisponible,
                     FechaFabricacion = producto.fechaFabricacion,
                     IdCategoria = producto.idCategoria,
-                    IdUsuario = producto.idUsuario
+                    IdUsuario = producto.idUsuario,
+                    PrecioBase = producto.precioBase,
+                    EsSubasta = producto.esSubasta,
+                    FechaLimite = producto.fechaLimite
                 };
                 foreach (var foto in producto.fotos)
                 {
@@ -74,6 +77,40 @@ namespace RetroShop_Client.Controllers
                 var response = await _service.getByFilterAsync(productoFilter);
                 if (response.Productos.Count == 0) return NoContent();
                 return Ok(response.Productos);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return StatusCode(500);
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] Producto producto)
+        {
+            try
+            {
+                ProductoDTO productoDTO = new ProductoDTO()
+                {
+                    IdProducto = producto.idProducto,
+                    Nombre = producto.nombre,
+                    Descripcion = producto.descripcion,
+                    Precio = producto.precio,
+                    CantidadDisponible = producto.cantidadDisponible,
+                    FechaFabricacion = producto.fechaFabricacion,
+                    IdCategoria = producto.idCategoria,
+                    IdUsuario = producto.idUsuario,
+                    PrecioBase = producto.precioBase,
+                    EsSubasta = producto.esSubasta,
+                    FechaLimite = producto.fechaLimite
+                };
+                foreach (var foto in producto.fotos)
+                {
+                    FotoDTO fotoDTO = new FotoDTO() { Nombre = foto.nombre, File = foto.file, IdFoto = foto.idFoto };
+                    productoDTO.Fotos.Add(fotoDTO);
+                }
+
+                var response = await _service.addProductoAsync(productoDTO);
+                return Ok(response.Producto);
             }
             catch (Exception ex)
             {
